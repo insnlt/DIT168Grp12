6f77a3307ab49234e1b5f1611164acc60e1b3788
Andrius <gussakan@student.gu.se>
2018-02-19 14:10:49 +0100

    build deletion

--- build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/insane/Desktop/project/DIT168Grp12/src
-# Build directory: /home/insane/Desktop/project/DIT168Grp12/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(src-Runner "/home/insane/Desktop/project/DIT168Grp12/build/src/src-Runner")
